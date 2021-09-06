56d4e173f003c41e08c2efe530eec5807c93be4c
jonahkiprop34@gmail.com <jonahkiprop34@gmail.com>
2021-09-06 13:17:18 +0300

    #declare const variables

--- src/datasources/swapi.js
+++ src/datasources/swapi.js
@@ -11,15 +11,15 @@ class SWAPI extends RESTDataSource {
         const {count, results, previous, next} = response;
         const pageSize = 10;
         const people =  Array.isArray(results) ? results.map(person => this.reducePerson(person)) : [];
-        let nextPage = !next ? null : next.split('=')[1];
-        let previousPage = ! previous ? null : previous.split('=')[1];
-        let hasMore = count > (10 * pageNumber);
-        let pageCount = Math.floor(count / pageSize) + (count % pageSize ? 1 : 0);
+        const nextPage = !next ? null : next.split('=')[1];
+        const previousPage = ! previous ? null : previous.split('=')[1];
+        const hasMore = count > (10 * pageNumber);
+        const pageCount = Math.floor(count / pageSize) + (count % pageSize ? 1 : 0);
         return { People: people, Pagination :{ hasMore, nextPage, previousPage, pageCount}};
     }
     async getPersonByName({name}) {
         const response = await this.get(`people?search=${name}`);
-        const {count, results, previous, next} = response;
+        const {results} = response;
         return Array.isArray(results) ? results.map(person => this.reducePerson(person)) : [];
     }
     reducePerson(person) {
